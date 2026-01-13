664b2de009050108c1ac184e27bbb1bf0d1d1a9e
Maria Lupiak <maria.lupiak@gmail.com>
2026-01-13 19:33:26 +0200

    add config model and json example

---
+++ DateTimePrinter/DateTimePrinter/Config.cs
@@ -0,0 +1,9 @@
+﻿namespace DateTimePrinter
+{
+    public class Config
+    {
+        public string Message { get; set; } = "";
+        public string DateFormat { get; set; } = "yyyy-MM-dd HH:mm:ss";
+        public int IntervalInSeconds { get; set; } = 5;
+    }
+}
